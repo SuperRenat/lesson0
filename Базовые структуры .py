091273e7061f28100567f206731bb49ec08f198f
SuperRenat <renat_083@mail.ru>
2024-07-22 21:49:40 +0700

    Add files via upload

---
+++ Базовые структуры .py
@@ -0,0 +1,37 @@
+print(5)
+print(type(5))
+print(5+5) # сложение
+print(5-5) # вычитание
+print(5*5) # умножение
+print(5/5) # деление
+print(5//5) # целочисленное деление
+print(5%5) # остаток от деления
+print(5%2) # остаток от деления
+print(6%2) # остаток от деления
+print(5**5) # возведение в степень
+print(2.0)
+print(type(2.0))
+print(2.0+2.0) # сложение дробных чисел
+print(2.0+2)
+print('Hello, World!') #concatenate
+print(type('Hello, World!')) #string - строка
+print("Hello, World!")
+print(type("Hello, World!"))
+print("Hello,"+"World!")
+print("1"+"1")
+print("3"+"5")
+print(True, False)
+print(type(True), type(False)) #boolean
+print(5 > 10)
+print(5 < 10)
+print(5 > 10, 10 > 5)
+print(5 == 5)
+print(5 == 6)
+print(5 != 5)
+print(5 != 6)
+print(5 == 5)
+print(5 != 5 and 5 < 10)
+print(5 == 5 and 5 < 10)
+print(5 != 5 or 5 < 10)
+print(int("5"))
+print(type(int("5")))
