daab3d48f81f1003e7706993c612b59362813979
SuperRenat <renat_083@mail.ru>
2024-07-23 00:19:02 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,17 @@
+name = "Renat"
+print(name)
+age = 40
+print(age)
+age = age + 1
+print(age)
+is_student = True
+print(is_student)
+
+name = "Renat"
+print("Name: " + name)
+age = 40
+print("Age: ", age)
+age = age + 1
+print("New Age: ", age)
+is_student = True
+print("Is Student: ", is_student)
