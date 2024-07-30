a36a63a408ef0591f9a363075d9c32ba37c08247
SuperRenat <renat_083@mail.ru>
2024-07-30 23:33:39 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,16 @@
+my_list = ['apple', 'banana', 'coconut' ,'peach' ,'c' ]
+print(my_list)
+# print(my_list[0], my_list[4]) - можно вот так вывести на экран
+print(my_list[0:5:4])
+# print(my_list[2], my_list[3], my_list[4]) - и тут тоже можно вот так вывести на экран
+print(my_list[2:])
+my_list[2] = 'kiwi'
+print(my_list)
+
+my_dict = {'Apple': "Яблоко", 'Banana': "Банан", 'Coconut': "Кокос", 'Peach': "Персик", 'Ananas': "Ананас", }
+print(my_dict)
+print('Перевод: ', my_dict['Apple'])
+print('Перевод: ', my_dict['Coconut'])
+my_dict['Orange'] = "Апельсин"
+# my_dict.update({'Orange': "Апельсин"}) - можно и вот так
+print(my_dict)
